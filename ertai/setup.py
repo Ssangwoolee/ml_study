f1af60c5265d15176651dfe87deefcc3e90c193c
ertai <ertai01@gmail.com>
2018-10-25 08:25:48 +0900

    Cython example 3

--- ertai/setup.py
+++ ertai/setup.py
@@ -1,10 +1,16 @@
 from distutils.core import setup
+"""from distutils.extension import Extension"""
 from Cython.Build import cythonize
 
 """setup(
     ext_modules = cythonize("helloworld.pyx")
-)"""
+)
 
 setup(
     ext_modules=cythonize("fib.pyx")
+)"""
+
+setup(
+    ext_modules=cythonize("primes.pyx")
 )
+
--- ertai/test.py
+++ ertai/test.py
@@ -1,4 +1,6 @@
-"""import helloworld"""
+import helloworld
 import fib
+import primes
 
 fib.fib(2000)
+primes.primes(10)
